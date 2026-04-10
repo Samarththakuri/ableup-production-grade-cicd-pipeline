b521f2f357bdd0e39d7e6f99ddaf3a01406eacb6
KaachiMitti <samarththakuri98@gmail.com>
2026-04-10 19:41:29 +0530

    mongouri problem fix

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/abelup";
+  const uri = process.env.MONGO_URI || "";
   try {
     await mongoose.connect(uri);
     console.log(`MongoDB connected`);
